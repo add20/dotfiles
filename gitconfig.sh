338fc5005d0516f6a9a5b94c2fbe3047b9c40071
add20 <add20g@gmail.com>
2012-07-06 14:09:56 +0900

    Add git alias: diff

--- gitconfig.sh
+++ gitconfig.sh
@@ -19,6 +19,8 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global alias.df diff
+git config --global alias.df "diff --cached"
 git config --global alias.unstage "reset HEAD --"
 git config --global alias.last "log -1 -p HEAD"
 git config --global alias.stash-unapply "!git stash show -p | git apply -R"
