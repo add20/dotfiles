b5cd6357f653e71de70bdf39349669ab2805b3b7
add20 <add20g@gmail.com>
2012-12-05 14:56:13 +0900

    Delete vim-pathogen.sh

--- vim-pathogen.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p .vim/autoload .vim/bundle
-
-url=https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
-
-echo "getting the $url"
-curl -o .vim/autoload/pathogen.vim "$url"
-
-unset url
