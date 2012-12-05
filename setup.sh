4ab0391942dadce2d892472a481a78bf8f13875a
add20 <add20g@gmail.com>
2012-12-05 13:21:20 +0900

    Add setup.sh and .gitconfig

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+DOT_FILES=( .bash_profile .zshrc .gemrc .gitconfig .gitignore .gitmodules .inputrc .vimrc .vim .emacs .rvmrc .sqliterc)
+
+for file in ${DOT_FILES[@]}
+do
+    ln -s $HOME/dotfiles/$file $HOME/$file
+done
