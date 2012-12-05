496388537c40351fd08a97fe37e43fc05dfe91e3
add20 <add20g@gmail.com>
2012-12-05 14:45:14 +0900

    Change setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,8 @@ for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+# for NeoBundle
+mkdir -p ~/dotfiles/.vim/bundle
+git clone git://github.com/Shougo/neobundle.vim.git ~/dotfiles/.vim/bundle/neobundle.vim
+vim -u ~/dotfiles/.vimrc.bundle +NeoBundleInstall! +q
