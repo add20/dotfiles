d4f9aaae050e28487d5d0784a76113364e535aee
add20 <add20g@gmail.com>
2013-04-22 15:59:17 +0900

    Delete virtualenv

--- .zsh/export.zsh
+++ .zsh/export.zsh
@@ -6,10 +6,6 @@ HISTCONTROL=ignoredups
 # Make some commands not show up in history
 export HISTIGNORE="ls:ls *:cd:cd -:pwd;exit:date:* --help"
 
-# Python virtualenvwrapper
-export WORKON_HOME=$HOME/.virtualenvs
-source `which virtualenvwrapper.sh`
-
 # Vim
 export EDITOR=/Applications/MacVim.app/Contents/MacOS/Vim
 
