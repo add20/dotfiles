b73813c8caee9e69ff53c1d6ec6a5e369ed58cb0
add20 <add20g@gmail.com>
2013-04-27 16:38:30 +0900

    Change setup.sh add oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,12 @@ do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
+# for oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+rm -fr ~/.oh-my-zsh/custom
+ln -s ~/dotfiles/.zsh/plugins ~/.oh-my-zsh/custom
+ln -s ~/dotfiles/.zsh/themes/add20.zsh-theme ~/.oh-my-zsh/themes/add20.zsh-theme
+
 # for NeoBundle
 mkdir -p ~/dotfiles/.vim/bundle
 git clone git://github.com/Shougo/neobundle.vim.git ~/dotfiles/.vim/bundle/neobundle.vim
