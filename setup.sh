79b1639c3ef0b46f3eb3eb69e4782a9088016218
add20 <add20g@gmail.com>
2013-05-03 12:25:15 +0900

    Change setup.sh first clone oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
+# for oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+ln -s ~/dotfiles/.zsh/plugins ~/.oh-my-zsh/custom/
+ln -s ~/dotfiles/.zsh/themes ~/.oh-my-zsh/custom/
+
 DOT_FILES=( bin .tmux.conf .zsh .zshrc .zshenv .gemrc .gitconfig .gitignore .inputrc .vimrc .vim .emacs .sqliterc .ghci)
 
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
-
-# for oh-my-zsh
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-ln -s ~/dotfiles/.zsh/plugins ~/.oh-my-zsh/custom/
-ln -s ~/dotfiles/.zsh/themes ~/.oh-my-zsh/custom/
 
 # for NeoBundle
 mkdir -p ~/dotfiles/.vim/bundle
