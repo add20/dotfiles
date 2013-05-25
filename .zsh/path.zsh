91d20594d6c0bfadb3af648a1039a39a49726b29
add20 <add20g@gmail.com>
2013-05-25 09:58:31 +0900

    # This is a combination of 2 commits. # The first commit's message is:
    Add pkgconfig
    
    # This is the 2nd commit message:
    
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    # Not currently on any branch.
    # You are currently editing a commit while rebasing branch 'develop' on 'e567727'.
    #
    # Changes to be committed:
    #   (use "git reset HEAD^1 <file>..." to unstage)
    #
    # modified:   .zsh/path.zsh
    #
    # Untracked files:
    #   (use "git add <file>..." to include in what will be committed)
    #
    # bin/subl
    # bin/vimcat

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -5,9 +5,17 @@ export PATH=/usr/local/sbin:$PATH
 export PATH=/usr/local/bin:$PATH
 export PATH=$HOME/bin:$PATH
 
-# neocomplcache-ghc (neco-ghc) for Vim
 if [[ $OSTYPE == darwin* ]]; then
+  # neocomplcache-ghc (neco-ghc) for Vim
   export PATH=$HOME/Library/Haskell/bin:$PATH
+
+  # for gtk2hs and gtk cabal package
+  # http://www.haskell.org/haskellwiki/Gtk2Hs/Mac
+  export PKG_CONFIG_PATH=/usr/local/opt/glade/lib/pkgconfig:$PKG_CONFIG_PATH
+  export PKG_CONFIG_PATH=/usr/local/opt/cairo/lib/pkgconfig:$PKG_CONFIG_PATH
+  export PKG_CONFIG_PATH=/usr/local/opt/glib/lib/pkgconfig:$PKG_CONFIG_PATH
+  export PKG_CONFIG_PATH=/usr/local/opt/gtk+/lib/pkgconfig:$PKG_CONFIG_PATH
+  export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig:$PKG_CONFIG_PATH
 fi
 
 export PATH=$HOME/.cabal/bin:$PATH
