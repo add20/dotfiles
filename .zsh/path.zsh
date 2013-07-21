bba3954be154beec0226d8f837120e72ccd7e875
add20 <add20g@gmail.com>
2013-07-21 15:08:47 +0900

    Delete comment for $PKG_CONFIG_PATH

--- .zsh/path.zsh
+++ .zsh/path.zsh
@@ -8,14 +8,6 @@ export PATH=$HOME/bin:$PATH
 if [[ $OSTYPE == darwin* ]]; then
   # neocomplcache-ghc (neco-ghc) for Vim
   export PATH=$HOME/Library/Haskell/bin:$PATH
-
-  # for gtk2hs and gtk cabal package
-  # http://www.haskell.org/haskellwiki/Gtk2Hs/Mac
-  # export PKG_CONFIG_PATH=/usr/local/opt/glade/lib/pkgconfig:$PKG_CONFIG_PATH
-  # export PKG_CONFIG_PATH=/usr/local/opt/cairo/lib/pkgconfig:$PKG_CONFIG_PATH
-  # export PKG_CONFIG_PATH=/usr/local/opt/glib/lib/pkgconfig:$PKG_CONFIG_PATH
-  # export PKG_CONFIG_PATH=/usr/local/opt/gtk+/lib/pkgconfig:$PKG_CONFIG_PATH
-  # export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig:$PKG_CONFIG_PATH
 fi
 
 export PATH=$HOME/.cabal/bin:$PATH
