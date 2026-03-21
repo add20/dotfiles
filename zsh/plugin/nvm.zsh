23341d0da5ddd587adbf716788f804bcc293ee57
add20 <add20g@gmail.com>
2026-03-21 22:51:04 +0900

    Update nvm.zsh

--- zsh/plugin/nvm.zsh
+++ zsh/plugin/nvm.zsh
@@ -1,16 +1,13 @@
 # source ~/.nvm/nvm.sh
 
 # You should create NVM's working directory if it doesn't exist:
-
 [ -d ~/.nvm ] || mkdir ~/.nvm
 
-# Add the following to ~/.zshrc or your desired shell
-# configuration file:
-
+# Add the following to your shell profile e.g. ~/.profile or ~/.zshrc:
 export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 # You can set $NVM_DIR to any location, but leaving it unchanged from
-# /usr/local/Cellar/nvm/0.38.0 will destroy any nvm-installed Node installations
+# /opt/homebrew/Cellar/nvm/0.40.4 will destroy any nvm-installed Node installations
 # upon upgrade/reinstall.
